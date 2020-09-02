4b6451c1a9c4c121382788b6c4ffed1c0b099f2c
poweruser <C:\Users\1\AppData\Roaming\The Bat!>
2020-09-02 18:08:03 +0300

    added react-dom-routre and dynamic loading from firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import './App.css';
-import Sidebar from './Sidebar';
-import Chat from './Chat';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Login from './Login';
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState('User1');
   return (
     <div className="app">
-      <div className="app__body">
-        <Sidebar />
-        <Chat />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
